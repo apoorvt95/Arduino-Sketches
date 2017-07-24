05e9d3467252d6fb6914bfef489dd65b97b94a3c
Apoorv Tomar <apoorv.tomar95@gmail.com>
2017-07-24 10:41:25 +0530

    Updated ino

--- Automated_sim800/Automated_sim800.ino
+++ Automated_sim800/Automated_sim800.ino
@@ -1,21 +1,32 @@
+// Made By Apoorv Tomar
+// This sketch is used to establish communication between various 
+// peripheral devices connected to Arduino
+// Devices connected : SIM800, HC-05 (in Master mode), MQ-135, 
+// Please refer to the bluetooth_master.ino file to operate HC-05 in master mode
+
+
 #include<SoftwareSerial.h>
 void setup() {
+// Serial Monitor started at 9600 baud rate
   Serial.begin(9600);
+// Hardware Serial communication started at Pins RX1 and TX1
   Serial1.begin(9600);
   delay(1000);
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int flag=0;
+  // Code to configure SIM800 for first time
   if(flag==0)
   {
   Serial1.write("at\r\n");
   if(Serial1.available())
   Serial.write(Serial1.read());
   delay(1000);
+  // Write your appropriate APN instead of "airtel"
   Serial1.write("at+sapbr=3,1,\"APN\",\"airtel\"\r\n");
   if(Serial1.available())
   Serial.write(Serial1.read());
@@ -32,7 +43,8 @@ void loop() {
   if(Serial1.available())
   Serial.write(Serial1.read());
   delay(1000);
-  Serial1.write("at+httppara=\"URL\",\"52.76.4.177/?v7&session=123&time=1141212312312312&kff1005=34&kff1006=345&kff1001=34\"\r\n");
+  // Give URL inplace of ENDPOINT_URL field;
+  Serial1.write("at+httppara=\"URL\",\"ENDPOINT_URL");
   if(Serial1.available())
   Serial.write(Serial1.read());
   delay(1000);
