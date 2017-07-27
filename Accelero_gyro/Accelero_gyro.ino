5a7d8254b1bf3e0ac659e889271891537fb6066f
Apoorv Tomar <apoorv.tomar95@gmail.com>
2017-07-27 13:04:58 +0530

    Changed acce.ino

--- Accelero_gyro/Accelero_gyro.ino
+++ Accelero_gyro/Accelero_gyro.ino
@@ -1,52 +1,51 @@
-//  by Apoorv Tomar
-//  HC-05 universal test sketch to enter AT-commands
-//  Uses hardware serial to talk to the host computer and software serial for communication with the bluetooth module
-//  BT VCC to Arduino 5V out.
-//  BT GND to GND
-//  Arduino D11 to BT RX
-//  Arduino D10 BT TX
-//  Key Pin D9
-//
-//  When a command is entered in the serial monitor on the computer
-//  the Arduino will relay it to the bluetooth module and display the result.
-//
-//  The HC-05 modules require both CarriageReturn CR and NewLine NL
-
-#include<SoftwareSerial.h>
-SoftwareSerial BTSerial(10, 11);
-
-const int keyPin = 9;
-
-String HC05_Responce = "";
-
-void setup() {
-
-  //output from keypin
-  pinMode(keyPin, OUTPUT);
-  digitalWrite(keyPin, HIGH);
-
-  Serial.begin(9600);
-  Serial.println("Sketch HC-05");
-  Serial.println("Arduino with HC-05 is ready");
-  Serial.println("Make sure Both NL & CR are set");
-  Serial.println("");
-
-  //Set to HC-05 default baud rate, usually 38400.
-  BTSerial.begin(38400);
-  Serial.println("BTserial started at 38400");
-  Serial.println("");
-
+include
+define X0 0x32
+define X1 0x33
+define Y0 0x34
+define Y1 0x35
+define Z0 0x36
+define Z1 0x37
+void setup()
+{
+Wire.begin(); // join i2c bus
+Serial.begin(19200); // start serial for output
+Wire.beginTransmission(0x1D); // transmit to ADXL345
+Wire.send(0x2D);// POWER_CTL
+Wire.send(0x09);// measurement mode, 4hz wakeup
+Wire.endTransmission(); // stop transmitting
+}
+void loop()
+{
+Serial.print(getDir(‘x’), DEC); // print as an ASCII-encoded decimal
+Serial.print(“\t”); // prints a tab
+Serial.print(getDir(‘y’), DEC);
+Serial.print(“\t”);
+Serial.print(getDir(‘z’), DEC);
+Serial.print(“\t”);
+Serial.println(“”); // prints a newline
+delay(300);
+}
+byte requestByte(char dir){
+Wire.beginTransmission(0x1D); // transmit to ADXL345
+Wire.send(dir);
+Wire.endTransmission(); // stop transmitting
+Wire.requestFrom(0x1D, 1); // request 1 byte from ADXL345
+while(Wire.available()){
+return(Wire.receive()); //
+}
+}
+int getDir(char dir){
+int var;
+if(dir==‘x’){
+var=requestByte(X0);
+var=var+(requestByte(X1)<<8);
+}else if(dir==‘y’){
+var=requestByte(Y0);
+var=var+(requestByte(Y1)<<8);
+}else if(dir==‘z’){
+var=requestByte(Z0);
+var=var+(requestByte(Z1)<<8);
+}
+return(var);
 }
 
-void loop() {
-  
-  // Read from the Bluetooth module and send to the Arduino Serial Monitor
-  if (BTSerial.available()) {
-    Serial.write(BTSerial.read());
-  }
-
-  // Read from the Serial Monitor and send to the Bluetooth module
-  if (Serial.available()) {
-    BTSerial.write(Serial.read());
-  }
-}
